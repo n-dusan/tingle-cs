acb34572870632c27cc4674261dbd058c9379e78
Dusan <nikolic.dusan.dey@gmail.com>
2020-04-24 01:49:58 +0200

    reverted jca

--- src/main/java/com/tingle/tingle/service/CertificateService.java
+++ src/main/java/com/tingle/tingle/service/CertificateService.java
@@ -435,21 +435,7 @@ public class CertificateService {
 		}
 
 	}
-
-	private byte[] encryptCertToPKCS7(X509Certificate certificate, Key key, X509Certificate[] certificates)
-			throws CertificateEncodingException, CMSException, NoSuchProviderException, NoSuchAlgorithmException, IOException, OperatorCreationException {
-		CMSSignedDataGenerator generator = new CMSSignedDataGenerator();
-
-		ContentSigner sha256Signer = new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC").build((PrivateKey) key);
-		generator.addSignerInfoGenerator(new JcaSignerInfoGeneratorBuilder(new JcaDigestCalculatorProviderBuilder()
-				.setProvider("BC").build())
-				.build(sha256Signer, certificate));
-		generator.addCertificates(new JcaCertStore(Arrays.stream(certificates).collect(Collectors.toList())));
-		CMSTypedData content = new CMSProcessableByteArray(certificate.getEncoded());
-
-		CMSSignedData signedData = generator.generate(content, true);
-		return signedData.getEncoded();
-	}
+	
 
 	public void revokeCertificate(String serialNumber, CRLReason reason)
 			throws NoSuchProviderException, CertificateException, NoSuchAlgorithmException, InvalidKeyException {
